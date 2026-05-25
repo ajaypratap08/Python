f7dbb6fafc891940ec510a49f3cdf16e46fd5e45
AJAY CHAND <apchand99@gmail.com>
2026-05-26 00:36:03 +0530

    First Commit

--- CH2_loops.py
+++ CH2_loops.py
@@ -1,6 +1,28 @@
 # Loops in Python
-num = 1;
-while num <= 5:
-    print(num)
-    num+=1
-    
+# num = 1;
+# while num <= 5:
+#     print(num)
+#     num+=1
+
+# while(num<=20):
+#     if(num%2!=0):
+#         print(num)
+        
+#     else:
+#         print("Even number ")  
+#     num+=1
+
+# i=1
+# while(i <=4):
+#     print("*" * i)
+#     i+=1
+
+# For loop
+# range (start, end, step 
+# for i in range (1,6,1):
+#     print(i)
+
+for i in range(5, -1, -1):
+    print(i)
+    if(i == 0):
+        print("Blastoff")
